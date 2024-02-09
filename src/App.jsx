143310da27f06c3d88f668a5e55eea235d3b872f
Amine Trigui <aminetrigui.github@gmail.com>
2024-02-09 20:01:21 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+export function App() {
+  return (
+    <>
+    <div className="text-4xl text-yellow-500 h-14 max-w-16 bg-green-500">App</div>
+    <div className="text-4xl text-yellow-500 h-14 max-w-16 bg-red-500">App</div>
+    <div className="text-4xl text-yellow-500 h-14 max-w-16 bg-blue-500">App</div>
+    </>
+    )
+}
+
