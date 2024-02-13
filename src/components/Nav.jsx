b830be4cc0e38be74c989f714affbfba83410dad
Amine Trigui <aminetrigui.github@gmail.com>
2024-02-13 20:19:51 +0100

    Hide menu and shopping btn

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
+import { TbShoppingBag } from "react-icons/tb"; 
 import NikeLogo from "../assets/nike-logo.svg?react";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu} from "react-icons/rx";
+import { useState } from "react";
 
 export function Nav() {
 
 const ROUTES = ["Home","About","Services","Pricing","Contact"]
-  return (
+
+const[isMobileMenuShow,setIsMobileMenuShow]= useState(false)
+
+return (
 
 <nav className="items-center flex justify-between flex-wrap" >
 
@@ -16,16 +21,22 @@ const ROUTES = ["Home","About","Services","Pricing","Contact"]
 
 
 {/* Hamburger button */}
-<button className="  rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+<button onClick={ ()=>setIsMobileMenuShow(!isMobileMenuShow)} className=" lg:hidden rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
 <RxHamburgerMenu size={25} />
 </button>
 
 {/* Menu List */}
-<div className="w-full">
-<ul className="bg-gray-100 rounded-lg border p-4 border-gray-100 text-lg ">{ROUTES.map((route, i )=>{ return <li key={i} className= {`cursor-pointer rounded px-3 py-2 ${(i === 0 )? "bg-blue-500 " : "hover:bg-gray-400"}`}>{route}</li> }) }
+<div className={`${isMobileMenuShow === false && "hidden"} w-full lg:w-auto lg:block`}>
+<ul className=" lg:space-x-8 lg:bg-transparent lg:border-none flex flex-col  lg:flex-row  bg-gray-100 rounded-lg border p-4 border-gray-100 text-lg ">{ROUTES.map((route, i )=>{ return <li key={i} className= {`cursor-pointer rounded px-3 py-2 ${(i === 0 )? "text-white lg:bg-transparent lg:text-blue-500 bg-blue-500 " : "hover:bg-gray-400"}`}>{route}</li> }) }
 </ul>
 </div>
 
+{/* Panier: cart button */}
+<div className="fixed bottom-4 left-4 lg:static">
+<div className="h-12 w-12 rounded-full flex-center bg-slate-400  shadow-md ">
+<TbShoppingBag />
+</div>
+</div>
 </nav>
     )
 }
